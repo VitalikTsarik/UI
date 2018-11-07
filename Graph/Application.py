fac81f44fd5082b38dac81a2fd802c3f346f69a2
Katia Sychik <katerinasychik@gmail.com>
2018-11-07 23:14:07 +0300

    delete unused ErrorMessage object

--- Graph/Application.py
+++ Graph/Application.py
@@ -1,9 +1,7 @@
 import sys
 from Graphics.Paint import *
-from Graphics.ErrorWindow import ErrorWindow
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     Window = MainWindow()
-    main_window_errors = ErrorWindow(Window)
     sys.exit(app.exec_())
