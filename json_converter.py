e8e071cc19b1f3561e4166622d7cc89357fab491
Katia Sychik <katerinasychik@gmail.com>
2018-11-18 23:44:12 +0300

    add converting dict to graph

--- json_converter.py
+++ json_converter.py
@@ -1,5 +1,6 @@
 import json
 from graph import Graph
+from game_components import Train
 
 
 def read_graph_from_json(filename):
@@ -15,3 +16,13 @@ def dict_to_graph(dictionary):
     for line in dictionary['lines']:
         graph.add_edge(idx=line['idx'], length=line['length'], vert_from=line['points'][0], vert_to=line['points'][1])
     return graph
+
+
+def dict_to_trains(dictionary):
+    trains = dictionary['trains']
+    return [dict_to_train(train) for train in trains]
+
+
+def dict_to_train(dictionary):
+    return Train(idx=dictionary['idx'], speed=dictionary['speed'], level=dictionary['level'],
+                 line_idx=dictionary['line_idx'], position=dictionary['position'], player_idx=dictionary['player_idx'])
