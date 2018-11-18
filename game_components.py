571c0af42648e3d704892ca52a3c98cd2a5e6a58
Katia Sychik <katerinasychik@gmail.com>
2018-11-18 23:40:15 +0300

    add player_idx for train

--- game_components.py
+++ game_components.py
@@ -1,5 +1,5 @@
 class Train:
-    def __init__(self, idx, speed, level, line_idx, position):
+    def __init__(self, idx, speed, level, line_idx, position, player_idx):
         self.__idx = idx
         self.__speed = speed
         self.__level = level
@@ -7,6 +7,7 @@ class Train:
         self.__line_idx = line_idx
         self.__position = position
 
+        self.__player_idx = player_idx
 
     @property
     def idx(self):
@@ -28,6 +29,10 @@ class Train:
     def position(self):
         return self.__position
 
+    @property
+    def player_idx(self):
+        return self.__player_idx
+
     @line_idx.setter
     def line_idx(self, value):
         self.__line_idx = value
@@ -35,7 +40,6 @@ class Train:
     @position.setter
     def position(self, value):
         self.__position = value
-
 
 
 class GamePoint:
