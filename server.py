7eb934d0dc6145362c7462e8bdd63fe02f4f58f8
Katia Sychik <katerinasychik@gmail.com>
2018-11-18 19:16:29 +0300

    change syntax and class name

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socket
-class server:
-    
+
+
+class Server:
     def login(self, person_name):
         with socket.socket() as s:
             s.connect(('wgforge-srv.wargaming.net', 443))
@@ -24,58 +25,55 @@ class server:
             res = s.recv(4)
             size = int.from_bytes(s.recv(4), byteorder='little')
             d = s.recv(size)
-    
 
     def logout(self):
         with socket.socket() as s:
             s.send(b'\x02\x00\x00\x00\x00\x00\x00\x00')
 
 
-
-
 def map_get(self,layer_num):
     with socket.socket() as s:
         action = 10
-            action_byte = (action).to_bytes(4, byteorder='little')
+        action_byte = (action).to_bytes(4, byteorder='little')
             
-            msg_fp = "{\"layer\":"
-            layer_num = '0'
-            msg_lp = "}"
+        msg_fp = "{\"layer\":"
+        layer_num = '0'
+        msg_lp = "}"
             
-            msg = msg_fp + layer_num + msg_lp
-            msg_len = len(str(msg))
+        msg = msg_fp + layer_num + msg_lp
+        msg_len = len(str(msg))
             
-            msg_len_byte = (msg_len).to_bytes(4, byteorder='little')
+        msg_len_byte = (msg_len).to_bytes(4, byteorder='little')
             
-            msg_byte = msg.encode()
-            msg_serv = action_byte + msg_len_byte + msg_byte
+        msg_byte = msg.encode()
+        msg_serv = action_byte + msg_len_byte + msg_byte
             
-            s.send(msg_serv)
+        s.send(msg_serv)
             
-            res = s.recv(4)
-            size = int.from_bytes(s.recv(4), byteorder='little')
-            d = s.recv(size)
+        res = s.recv(4)
+        size = int.from_bytes(s.recv(4), byteorder='little')
+        d = s.recv(size)
 
 
 def move(self, line_idx, speed, train_idx):
     with socket.socket() as s:
         action = 3
-            action_byte = (action).to_bytes(4, byteorder='little')
+        action_byte = (action).to_bytes(4, byteorder='little')
             
-            msg_fp = "{\"line_idx\":"
-            line_idx = '193'
-            msg_mp = ",\"speed\":"
-            speed = '1'
-            msg_mmp = ",\"train_idx\":"
-            train_idx = '1'
-            msg_lp = "}"
+        msg_fp = "{\"line_idx\":"
+        line_idx = '193'
+        msg_mp = ",\"speed\":"
+        speed = '1'
+        msg_mmp = ",\"train_idx\":"
+        train_idx = '1'
+        msg_lp = "}"
             
-            msg = msg_fp + line_idx + msg_mp + speed + msg_mmp + train_idx + msg_lp
-            msg_len = len(str(msg))
+        msg = msg_fp + line_idx + msg_mp + speed + msg_mmp + train_idx + msg_lp
+        msg_len = len(str(msg))
             
-            msg_len_byte = (msg_len).to_bytes(4, byteorder='little')
+        msg_len_byte = (msg_len).to_bytes(4, byteorder='little')
             
-            msg_byte = msg.encode()
-            msg_serv = action_byte + msg_len_byte + msg_byte
+        msg_byte = msg.encode()
+        msg_serv = action_byte + msg_len_byte + msg_byte
             
-            s.send(msg_serv)
+        s.send(msg_serv)
