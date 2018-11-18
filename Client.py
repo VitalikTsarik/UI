79449a4cc7d29e5220a5abad90a1aae41c9c1289
VitalikTsarik <vitamin10101@gmail.com>
2018-11-18 23:22:22 +0300

    added request, response methods

--- Client.py
+++ Client.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
+import socket
 from enum import Enum
+from json import dumps, loads
+from struct import pack, unpack
+import time
 
 SERVER = 'wgforge-srv.wargaming.net'
 PORT = 443
@@ -25,7 +29,27 @@ class Result(Enum):
     INTERNAL_SERVER_ERROR = 500
 
 
-class Layer(Enum):
-    Layer0 = 0
-    Layer1 = 1
-    Layer10 = 10
+class ServerConnection:
+    def __init__(self, server, port):
+        self.__socket = socket.socket()
+        self.__socket.connect((server, port))
+
+    def __request(self, action, data=None):
+        if data:
+            msg = pack('ii', action.value, len(data)) + data.encode('UTF-8')
+        else:
+            msg = pack('ii', action.value, 0)
+        self.__socket.send(msg)
+
+    def __response(self):
+        res = unpack('i', self.__socket.recv(4))[0]
+        if res == 0:
+            size = unpack('i', self.__socket.recv(4))[0]
+            if size:
+                data = b''
+                while size > 128:
+                    data += self.__socket.recv(128)
+                    size -= 128
+                data += self.__socket.recv(size)
+                return loads(data.decode('UTF-8'))
+        return res
