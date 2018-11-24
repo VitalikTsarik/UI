4fb00faea28f08b7d449c47d46473dba7ec3124f
VitalikTsarik <vitamin10101@gmail.com>
2018-11-24 18:38:41 +0300

    added choose direction

--- game_components/game.py
+++ game_components/game.py
@@ -18,12 +18,26 @@ class Game:
             road = self.map_graph.get_edge_by_idx(train.line_idx)
             if train.position == 0 or train.position == road['length']:
                 train.speed = 0
+                edge = self.__map_graph.get_edge_by_idx(train.line_idx)
+                if train.position == 0:
+                    self.__choose_direction(edge['vert1'])
+                else:
+                    self.__choose_direction(edge['vert2'])
 
     def move_train(self, train_idx, line_idx, speed):
         res, msg = self.__client.move_action(train_idx, line_idx, speed)
         if res == Result.OKEY.value:
             self.__trains[train_idx].line_idx = line_idx
             self.__trains[train_idx].speed = speed
+
+    def __choose_direction(self, vert_idx):
+        self.__main_window.centralWidget().create_dir_btns(self.__map_graph.get_adj_vertices(vert_idx))
+        self.__main_window.centralWidget().add_dir_btns()
+
+    def set_direction(self):
+        # получение line_idx
+        # вызов move_train
+        self.__main_window.centralWidget().del_dir_btns()
 
     @property
     def map_graph(self):
