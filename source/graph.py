b26feb6de38c16464cc1d5b794171a903fb874c6
Katia Sychik <katerinasychik@gmail.com>
2018-12-01 19:22:19 +0300

    rename field adj_edge to adj_edges in each vertex if graph

--- source/graph.py
+++ source/graph.py
@@ -22,7 +22,7 @@ class Graph(object):
         if idx not in self.__graph.keys():
             self.__graph[idx] = {
                 'post_idx': post_idx,
-                'adj_edge': []
+                'adj_edges': []
             }
 
     # warning попытка добавить existing edge
@@ -30,13 +30,13 @@ class Graph(object):
         if vert_from == vert_to:
             return
         if vert_from in self.__graph.keys():
-            for vertex in self.__graph[vert_from]['adj_edge']:
+            for vertex in self.__graph[vert_from]['adj_edges']:
                 if vertex['vert_to'] == vert_to:
                     return
         else:
             self.add_vertex(vert_from)
 
-        self.__graph[vert_from]['adj_edge'].append(
+        self.__graph[vert_from]['adj_edges'].append(
             {
                 'edge_idx': idx,
                 'length': length,
@@ -47,7 +47,7 @@ class Graph(object):
 
         if vert_to not in self.__graph.keys():
             self.add_vertex(vert_to)
-        self.__graph[vert_to]['adj_edge'].append(
+        self.__graph[vert_to]['adj_edges'].append(
             {
                 'edge_idx': idx,
                 'length': length,
@@ -60,24 +60,24 @@ class Graph(object):
         return tuple(self.__graph.keys())
 
     def get_adj_edges(self, idx):
-        return self.__graph[idx]['adj_edge']
+        return self.__graph[idx]['adj_edges']
 
     def get_adj_vertices(self, idx):
-        return tuple([edge['vert_to'] for edge in self.__graph[idx]['adj_edge']])
+        return tuple([edge['vert_to'] for edge in self.__graph[idx]['adj_edges']])
 
     def get_vertex_post_idx(self, idx):
         return self.__graph[idx]['post_idx']
 
     def get_edge_by_idx(self, idx):
         for vert in self.__graph.keys():
-            for edge in self.__graph[vert]['adj_edge']:
+            for edge in self.__graph[vert]['adj_edges']:
                 if edge['edge_idx'] == idx:
                     return {'length': edge['length'],
                             'vert_from': edge['start_vert'],
                             'vert_to': edge['vert_to'] if edge['start_vert'] != edge['vert_to'] else vert}
 
     def get_edge_by_adj_vert(self, vert1, vert2):
-        for edge in self.__graph[vert1]['adj_edge']:
+        for edge in self.__graph[vert1]['adj_edges']:
             if edge['vert_to'] == vert2:
                 return {'length': edge['length'],
                         'edge_idx': edge['edge_idx'],
