e78d74ae6bace709228fbac486ead3a67319cb1e
VitalikTsarik <vitamin10101@gmail.com>
2018-12-01 21:02:44 +0300

    added __calc_path method

--- source/game_components/path.py
+++ source/game_components/path.py
@@ -1,11 +1,14 @@
 class Path:
     def __init__(self):
-        self.__vertices = [14, 20, 19, 13]
+        self.__calc_path()
+
+    def __calc_path(self):
+        self.__vertices = [14, 20, 19, 13]  # todo: вставить алгоритм просчёта пути
         self.__i = 0
 
     def next_vert(self):
+        if self.__i == len(self.__vertices):
+            self.__calc_path()
         vert = self.__vertices[self.__i]
         self.__i += 1
-        if self.__i == len(self.__vertices):
-            self.__i = 0
         return vert
