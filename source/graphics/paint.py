6aa1d4896cef18f2dfba08d4ab0e66b51f38a7bb
VitalikTsarik <vitamin10101@gmail.com>
2018-12-08 17:43:41 +0300

    added storages visualization

--- source/graphics/paint.py
+++ source/graphics/paint.py
@@ -160,6 +160,7 @@ class PaintGraphWidget(QWidget):
         self.draw_vertices(h_painter, points, vert_radius)
         self.draw_town(h_painter, self.parent().parent().game.town, points, vert_radius)
         self.draw_markets(h_painter, self.parent().parent().game.markets, points, vert_radius)
+        self.draw_storages(h_painter, self.parent().parent().game.storages, points, vert_radius)
         self.draw_trains(h_painter, self.parent().parent().game.trains, points, vert_radius)
 
         h_painter.end()
@@ -253,6 +254,16 @@ class PaintGraphWidget(QWidget):
             rect = QRectF(x, y, 2 * radius, radius * 9 / 5)
             h_painter.drawText(rect, Qt.AlignHCenter | Qt.AlignBottom,
                                '+{}  {}/{}'.format(market.replenishment, market.product, market.product_capacity))
+
+    def draw_storages(self, h_painter, storages, points, radius):
+        pixmap = QPixmap("source\icons\storage.png").scaled(2 * radius, 2 * radius, Qt.KeepAspectRatio)
+        for storage in storages.values():
+            x = points[storage.point_idx].x() - radius
+            y = points[storage.point_idx].y() - radius
+            h_painter.drawPixmap(QPointF(x, y), pixmap)
+            rect = QRectF(x, y, 2 * radius, radius * 9 / 5)
+            h_painter.drawText(rect, Qt.AlignHCenter | Qt.AlignBottom,
+                               '+{}  {}/{}'.format(storage.replenishment, storage.armor, storage.armor_capacity))
 
     def draw_trains(self, h_painter, trains, points, vert_radius):
         h_painter.setBrush(self.train_color)
